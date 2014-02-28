5fb5e20e99bad49fbdfe99880edfa6a8ba8469ee
bbai <bingbaiatisu@gmail.com>
2014-02-28 13:19:53 -0500

    Add analyzedata method, reorgainze some of the methods

--- MongoDB/MongoDB.cs
+++ MongoDB/MongoDB.cs
@@ -14,23 +14,29 @@ namespace MongoDB
     public class MongoDBHelper
     {
         public MongoCollection<BsonDocument> collection;
-        public MongoDBHelper(string serverAddr, string dbName, string collectionName)
+        public Hashtable successAllConfigTable;
+        public Hashtable failAllConfigTable;
+        public Hashtable successConfigTable;
+        public Hashtable failConfigTable;
+        private DateTime mDate;
+        public MongoDBHelper(string serverAddr, string dbName, string collectionName, int days)
         {
             MongoClient client = new MongoClient(serverAddr);
             MongoServer server = client.GetServer();
             MongoDatabase mongoDB = server.GetDatabase(dbName);
             collection = mongoDB.GetCollection(collectionName);
+            mDate = DateTime.Now.Subtract(TimeSpan.FromDays(days));
         }
-
-        public MongoCursor<BsonDocument> GetCursorinDays(int days)
+        /// <summary>
+        /// Get the number of total test runs and failed test runs
+        /// </summary>
+        /// <returns>array contains total test runs and failed test run
+        ///          index 0 is total, 1 is fail</returns>
+        public int[] GetNumTotalAndFail()
         {
-            DateTime date = DateTime.Now.Subtract(TimeSpan.FromDays(days));
-            var query = Query.And(Query.GT("Date", date));
-            return collection.Find(query).SetSkip(1);
-        }
-
-        public int[] GetNumTotalAndFail(MongoCursor<BsonDocument> cursor)
-        {
+            var dateQuery = Query.And(Query.GT("Date", mDate));
+            //skip first inconsistant entry in current db.
+            MongoCursor<BsonDocument> cursor = collection.Find(dateQuery).SetSkip(1);
             int numTotalTests = 0;
             int numFailureTests = 0;
             int[] ret = new int[2];
@@ -47,6 +53,12 @@ namespace MongoDB
             ret[1] = numFailureTests;
             return ret;
         }
+
+        /// <summary>
+        /// Get project-automation hashtable, key is the project name, 
+        /// automations associated with projectname are the values(type of ArrayList)
+        /// </summary>
+        /// <returns>Hashtable that contains project-automation data</returns>
         public Hashtable GetProjectAutomationTable()
         {
             var projectNames = collection.Distinct("test-results.@project-name").ToList();
@@ -84,5 +96,137 @@ namespace MongoDB
             return projectAutomationTable;
         }
 
+        /// <summary>
+        /// Analyze the data in the Mongodb, and put associated data in to different tables
+        /// </summary>
+        /// <returns>Returns true when finshed analyzing data</returns>
+        public bool AnalyzeData()
+        {
+            bool ret = false;
+            successAllConfigTable = new Hashtable();
+            failAllConfigTable = new Hashtable();
+            successConfigTable = new Hashtable();
+            failConfigTable = new Hashtable();
+            var projectNames = collection.Distinct("test-results.@project-name").ToList();
+            Hashtable projectAutomationTable = this.GetProjectAutomationTable();
+            foreach (string project in projectNames)
+            {
+                ArrayList automationNameList = (ArrayList)projectAutomationTable[project];
+                Console.WriteLine(project);
+                foreach (string automaion in automationNameList)
+                {
+                    var queryResults1 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.ElemMatch("test-results.test-suite.results.test-case",
+                                        Query.And(Query.EQ("@name", automaion),
+                                        Query.EQ("@success", "True"))));
+                    MongoCursor<BsonDocument> testSuccess = collection.Find(queryResults1);
+                    if (testSuccess.Count() == 0)
+                    {
+                        queryResults1 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.And((Query.EQ("test-results.test-suite.results.test-case.@name", automaion)),
+                                        Query.EQ("test-results.test-suite.results.test-case.@success", "True")));
+                        testSuccess = collection.Find(queryResults1);
+                    }
+                    var queryResults2 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.ElemMatch("test-results.test-suite.results.test-case",
+                                        Query.EQ("@name", automaion)));
+                    MongoCursor<BsonDocument> testAll = collection.Find(queryResults2);
+                    if (testAll.Count() == 0)
+                    {
+                        queryResults2 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.And(Query.EQ("test-results.test-suite.results.test-case.@name", automaion)));
+                        testAll = collection.Find(queryResults2);
+                    }
+                    var queryResults3 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.ElemMatch("test-results.test-suite.results.test-case",
+                                        Query.And(Query.EQ("@name", automaion),
+                                        Query.EQ("@success", "False"))));
+                    MongoCursor<BsonDocument> testFail = collection.Find(queryResults3);
+                    if (testFail.Count() == 0)
+                    {
+                        queryResults3 = Query.And(Query.GT("Date", mDate), Query.EQ("test-results.@project-name", project),
+                                        Query.And((Query.EQ("test-results.test-suite.results.test-case.@name", automaion)),
+                                        Query.EQ("test-results.test-suite.results.test-case.@success", "False")));
+                        testFail = collection.Find(queryResults3);
+                    }
+                    if (testSuccess.Count() != testAll.Count() && testSuccess.Count() != 0)
+                    {
+                        IEnumerable<BsonDocument> diffs = testAll.Except(testSuccess);
+                        foreach (BsonDocument diffBson in diffs)
+                        {
+                            BsonDocument testResults = diffBson["test-results"].AsBsonDocument;
+                            string key = testResults["@template-name"].AsString;
+                            if (failConfigTable.ContainsKey(key) == false)
+                            {
+                                ArrayList list = new ArrayList();
+                                list.Add(automaion);
+                                failConfigTable.Add(key, list);
+                            }
+                            else
+                            {
+                                ArrayList list = (ArrayList)failConfigTable[key];
+                                list.Add(automaion);
+                                failConfigTable[key] = list;
+                            }
+                        }
+                    }
+                    if (testAll.Count() != testFail.Count() && testFail.Count() != 0)
+                    {
+                        IEnumerable<BsonDocument> diff = testAll.Except(testFail);
+                        foreach (BsonDocument diffBson in diff)
+                        {
+                            BsonDocument testResults = diffBson["test-results"].AsBsonDocument;
+                            string key = testResults["@template-name"].AsString;
+                            if (successConfigTable.ContainsKey(key) == false)
+                            {
+                                ArrayList list = new ArrayList();
+                                list.Add(automaion);
+                                successConfigTable.Add(key, list);
+                            }
+                            else
+                            {
+                                ArrayList list = (ArrayList)successConfigTable[key];
+                                list.Add(automaion);
+                                successConfigTable[key] = list;
+                            }
+                        }
+                    }
+                    if (testFail.Count() == 0)
+                    {
+
+                        if (successAllConfigTable.ContainsKey(project) == false)
+                        {
+                            ArrayList list = new ArrayList();
+                            list.Add(automaion);
+                            successAllConfigTable.Add(project, list);
+                        }
+                        else
+                        {
+                            ArrayList list = (ArrayList)successAllConfigTable[project];
+                            list.Add(automaion);
+                            successAllConfigTable[project] = list;
+                        }
+                    }
+
+                    if (testSuccess.Count() == 0)
+                    {
+                        if (failAllConfigTable.ContainsKey(project) == false)
+                        {
+                            ArrayList list = new ArrayList();
+                            list.Add(automaion);
+                            failAllConfigTable.Add(project, list);
+                        }
+                        else
+                        {
+                            ArrayList list = (ArrayList)failAllConfigTable[project];
+                            list.Add(automaion);
+                            failAllConfigTable[project] = list;
+                        }
+                    }
+                }
+            }
+            ret = true;
+            return ret;
+        }
     }
 }
